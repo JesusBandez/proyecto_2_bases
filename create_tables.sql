9515102df7c3ef41343477750be2d99c98a22d7b
Jesus_B <17-10046@usb.ve>
2023-04-12 13:51:19 -0400

    create tables

--- create_tables.sql
+++ create_tables.sql
@@ -2,82 +2,130 @@
     Nestor Gonzalez 16-10455
     Jesus Bandez 17-10046
 
-Crear la tabla inicial y se importan todos los datos del
-.csv a ella 
+Crear el esquema de la base
 */
 
-DROP TABLE file_data;
 
--- Crear tabla con el mismo formato donde se almacenaran los datos
-CREATE TABLE covid_data(
-    iso_code VARCHAR(255),
-    continent VARCHAR(255),
-    location VARCHAR(255),
-    date DATE,
-    total_cases DOUBLE PRECISION,
-    new_cases DOUBLE PRECISION,
-    new_cases_smoothed DOUBLE PRECISION,
-    total_deaths DOUBLE PRECISION,
-    new_deaths DOUBLE PRECISION,
-    new_deaths_smoothed DOUBLE PRECISION ,
-    total_cases_per_million DOUBLE PRECISION ,
-    new_cases_per_million DOUBLE PRECISION ,
-    new_cases_smoothed_per_million DOUBLE PRECISION ,
-    total_deaths_per_million DOUBLE PRECISION ,
-    new_deaths_per_million DOUBLE PRECISION ,
-    new_deaths_smoothed_per_million DOUBLE PRECISION ,
-    reproduction_rate DOUBLE PRECISION ,
-    icu_patients DOUBLE PRECISION ,
-    icu_patients_per_million DOUBLE PRECISION ,
-    hosp_patients DOUBLE PRECISION ,
-    hosp_patients_per_million DOUBLE PRECISION ,
-    weekly_icu_admissions DOUBLE PRECISION ,
-    weekly_icu_admissions_per_million DOUBLE PRECISION ,
-    weekly_hosp_admissions DOUBLE PRECISION ,
-    weekly_hosp_admissions_per_million DOUBLE PRECISION ,
-    total_tests DOUBLE PRECISION ,
-    new_tests DOUBLE PRECISION ,
-    total_tests_per_thousand DOUBLE PRECISION ,
-    new_tests_per_thousand DOUBLE PRECISION ,
-    new_tests_smoothed DOUBLE PRECISION ,
-    new_tests_smoothed_per_thousand DOUBLE PRECISION ,
-    positive_rate DOUBLE PRECISION ,
-    tests_per_case DOUBLE PRECISION ,
-    tests_units VARCHAR(255) ,
-    total_vaccinations DOUBLE PRECISION ,
-    people_vaccinated DOUBLE PRECISION ,
-    people_fully_vaccinated DOUBLE PRECISION ,
-    total_boosters DOUBLE PRECISION ,
-    new_vaccinations DOUBLE PRECISION ,
-    new_vaccinations_smoothed DOUBLE PRECISION ,
-    total_vaccinations_per_hundred DOUBLE PRECISION ,
-    people_vaccinated_per_hundred DOUBLE PRECISION ,
-    people_fully_vaccinated_per_hundred DOUBLE PRECISION ,
-    total_boosters_per_hundred DOUBLE PRECISION ,
-    new_vaccinations_smoothed_per_million DOUBLE PRECISION ,
-    new_people_vaccinated_smoothed DOUBLE PRECISION ,
-    new_people_vaccinated_smoothed_per_hundred DOUBLE PRECISION ,
-    stringency_index DOUBLE PRECISION ,
-    population_density DOUBLE PRECISION ,
-    median_age DOUBLE PRECISION ,
-    aged_65_older DOUBLE PRECISION ,
-    aged_70_older DOUBLE PRECISION ,
-    gdp_per_capita DOUBLE PRECISION ,
-    extreme_poverty DOUBLE PRECISION ,
-    cardiovasc_death_rate DOUBLE PRECISION ,
-    diabetes_prevalence DOUBLE PRECISION ,
-    female_smokers DOUBLE PRECISION ,
-    male_smokers DOUBLE PRECISION ,
-    handwashing_facilities DOUBLE PRECISION ,
-    hospital_beds_per_thousand DOUBLE PRECISION ,
-    life_expectancy DOUBLE PRECISION ,
-    human_development_index DOUBLE PRECISION ,
-    population DOUBLE PRECISION ,
-    excess_mortality_cumulative_absolute DOUBLE PRECISION ,
-    excess_mortality_cumulative DOUBLE PRECISION ,
-    excess_mortality DOUBLE PRECISION ,
-    excess_mortality_cumulative_per_million DOUBLE PRECISION
+DROP TABLE IF EXISTS item_in_box;
+DROP TABLE IF EXISTS box;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS unit;
+DROP TABLE IF EXISTS order_status;
+DROP TABLE IF EXISTS status_catalog;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS placed_order;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS city;
+
+
+-- yellow area
+
+CREATE TABLE unit (
+    id SERIAL PRIMARY KEY,
+    unit_name VARCHAR(64) NOT NULL,
+    unit_short VARCHAR(8)
 );
 
--- Importar datos en la tabla de sql creada
-\copy covid_data FROM 'owid-covid-data.csv' WITH DELIMITER ',' CSV HEADER
+CREATE TABLE item (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    item_photo TEXT, 
+    description TEXT, 
+    unit_id INT REFERENCES unit (id)
+);
+
+-- green area
+CREATE TABLE city (
+    id SERIAL PRIMARY KEY,
+    city_name VARCHAR(128), 
+    postal_code VARCHAR(16)
+);
+
+CREATE TABLE customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    user_name VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    time_inserted TIMESTAMP NOT NULL,
+    confirmation_code VARCHAR(128) NOT NULL,
+    time_confirmed TIMESTAMP,
+    contact_email VARCHAR(128) NOT NULL,
+    contact_phone VARCHAR(128),
+    city_id INT REFERENCES city (id),
+    address VARCHAR(255),
+    delivery_city_id INT REFERENCES city (id),
+    delivery_address VARCHAR(255)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    employee_code VARCHAR(32) NOT NULL,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL
+);
+
+-- RED AREA
+CREATE TABLE status_catalog (
+    id SERIAL PRIMARY KEY,
+    status_name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE placed_order (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customer (id) NOT NULL,
+    time_placed TIMESTAMP NOT NULL,
+    details TEXT,
+    delivery_city_id INT REFERENCES city (id) NOT NULL,
+    delivery_address VARCHAR(255) NOT NULL,
+    grade_customer INT,
+    grade_employee INT
+);
+
+CREATE TABLE order_status (
+    id SERIAL PRIMARY KEY,
+    placed_order_id INT REFERENCES placed_order (id) NOT NULL,
+    status_catalog_id INT REFERENCES status_catalog (id) NOT NULL,
+    status_time TIMESTAMP NOT NULL,
+    details TEXT
+);
+
+
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    placed_order_id INT REFERENCES placed_order (id) NOT NULL,
+    employee_id INT REFERENCES employee (id),
+    customer_id INT REFERENCES customer (id),
+    note_time TIMESTAMP  NOT NULL,
+    note_text text NOT NULL
+);
+
+CREATE TABLE delivery (
+    id SERIAL PRIMARY KEY,
+    delivery_time_planned TIMESTAMP NOT NULL,
+    delivery_time_actual TIMESTAMP,
+    notes TEXT,
+    placed_order_id INT REFERENCES placed_order (id) NOT NULL,
+    employee_id INT REFERENCES employee (id)
+);
+
+CREATE TABLE box(
+    id SERIAL PRIMARY KEY,
+    box_code VARCHAR(32) NOT NULL,
+    delivery_id INT REFERENCES delivery (id) NOT NULL,
+    employee_id INT REFERENCES employee (id) NOT NULL
+);
+
+
+CREATE TABLE item_in_box(
+    id SERIAL PRIMARY KEY,
+    box_id INT REFERENCES box (id) NOT NULL,
+    item_id INT REFERENCES item (id) NOT NULL,
+    quantity DECIMAL(10, 3) NOT NULL,
+    is_replacemen BOOLEAN NOT NULL
+);
+
+
